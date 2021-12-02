1aaa5028c2f6748ee790eb092d647247d1840fbd
Tyrone <zekah54@gmail.com>
2021-12-03 00:35:40 +0800

    to deploy

--- projects/utils.py
+++ projects/utils.py
@@ -5,11 +5,10 @@ from django.core.paginator import Paginator, PageNotAnInteger,EmptyPage
 def searchProjects(request):
     search_query = ''
 
-    if request.GET.get('query'):
+    if request.GET.get('query'):                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
         search_query = request.GET.get('query')
-
+        
     tags = Tag.objects.filter(name__icontains=search_query)
-
     projects = Project.objects.distinct().filter(
         Q(title__icontains=search_query) |
         Q(owner__name__icontains=search_query) |
